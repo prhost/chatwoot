aea850a1481fe0ff884655b86400966852fe0a0a
Pranav <pranavrajs@gmail.com>
2025-05-23 09:25:15 -0700

    Fix issues

--- enterprise/app/jobs/captain/copilot/response_job.rb
+++ enterprise/app/jobs/captain/copilot/response_job.rb
@@ -3,12 +3,18 @@ class Captain::Copilot::ResponseJob < ApplicationJob
 
   def perform(assistant:, conversation_id:, user_id:, copilot_thread_id:, message:)
     Rails.logger.info("[Captain::Copilot::BuildResponseJob] Starting job for user_id=#{user_id}")
-    generate_chat_response(assistant, conversation_id, user_id, copilot_thread_id, message)
+    generate_chat_response(
+      assistant: assistant,
+      conversation_id: conversation_id,
+      user_id: user_id,
+      copilot_thread_id: copilot_thread_id,
+      message: message
+    )
   end
 
   private
 
-  def generate_chat_response(assistant, conversation_id, user_id, copilot_thread_id, message)
+  def generate_chat_response(assistant:, conversation_id:, user_id:, copilot_thread_id:, message:)
     Captain::Copilot::ChatService.new(
       assistant,
       user_id: user_id,
