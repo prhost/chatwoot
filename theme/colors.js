381c1798150be2fcf396c3bb5d76b90b0d8ef3f5
Kallef Alexandre <kallef.alexandre@orbital.codes>
2025-07-20 23:08:28 -0300

    chore: Update docker-compose configuration for PostgreSQL service
    - Replaced port mapping with expose directive for the PostgreSQL service in the docker-compose file to enhance security and streamline service exposure.

--- theme/colors.js
+++ theme/colors.js
@@ -211,7 +211,7 @@ export const colors = {
     },
 
     black: '#000000',
-    brand: '#003F5C',
+    brand: '#2781F6',
     background: 'rgb(var(--background-color) / <alpha-value>)',
     solid: {
       1: 'rgb(var(--solid-1) / <alpha-value>)',
